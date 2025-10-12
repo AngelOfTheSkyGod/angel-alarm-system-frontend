51bb282e8fa33bf00820749eef6b5928e4719431
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2025-10-11 23:31:51 -0500

    connect to backend

--- src/hooks/useConfigContext.tsx
+++ src/hooks/useConfigContext.tsx
@@ -11,7 +11,7 @@ const ConfigContext = createContext<MyContextType | undefined>(undefined);
 
 const ConfigContextProvider = ({children}:{children:any}) => {
     const [config, setConfigContext] = useState({
-        baseUrl: "http://www.quinonesangel.com:8080"
+        baseUrl: "http://quinonesangel.com:8080"
     });
     const updateValue = (newValue: ConfigContextProps) => {
         setConfigContext(newValue);
