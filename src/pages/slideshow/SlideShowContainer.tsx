65cdf35ccb8e4dca9e293a3b7e0eb1ecd4bd2ac5
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2026-02-27 23:33:54 -0600

    log

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -130,7 +130,7 @@ export const SlideShowContainer = () => {
     useEffect(() => {
             console.log("updated effect", appData);
             setUpdatedData(slideShowDataToSlideShowDataWithBlob([...appData?.slideShowData || []]));
-    }, [appData.slideShowData])
+    }, [appData, imageCount])
 
     useEffect(() => {
         if (!appData?.username) {
