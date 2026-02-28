07cdd8bffb06659d3cb23cc0ac6dd0bb7675c8a3
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2026-02-27 23:47:57 -0600

    updated check

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -172,7 +172,7 @@ export const SlideShowContainer = () => {
                                 <DeleteIcon fontSize="inherit"/>
                             </IconButton>
                         }
-                        {imageCount > 0 && updatedData?.length >= imageCount &&
+                        {imageCount > 0 && updatedData?.length >= currentImageIndex &&
                             <img
                                 alt="not found"
                                 width={"100%"}
