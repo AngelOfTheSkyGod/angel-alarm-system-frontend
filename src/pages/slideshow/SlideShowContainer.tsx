3d25da7cb45e6938731ad9c9a4d119bcdc914a08
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2026-03-01 14:31:56 -0600

    reordering to get correct page number

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -51,9 +51,9 @@ export const SlideShowContainer = () => {
         console.log("current image index:", currentImageIndex, "image count:", imageCount, "pageNumber: ", pageNumber);
         if (currentImageIndex >= imagesLength - 1 && currentImageIndex < imageCount - 1){
             console.log("calling for more images...", pageNumber + 1)
-            callSlideShow({username: appData?.username, password: appData?.password, pageNumber: Math.floor(currentImageIndex / 3), startNumber: currentImageIndex + 1})
             setCurrentImageIndex(currentImageIndex + 1)
             setPageNumber(Math.floor(currentImageIndex / 3))
+            callSlideShow({username: appData?.username, password: appData?.password, pageNumber: Math.floor(currentImageIndex / 3), startNumber: currentImageIndex + 1})
             return;
         }else if (currentImageIndex >= imageCount - 1){
             console.log("resetting back to 0... image count:", imageCount, "current image index:", currentImageIndex)
