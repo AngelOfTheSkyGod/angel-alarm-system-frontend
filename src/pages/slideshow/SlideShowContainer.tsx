17ac9fcab29741d61357a42a9a9b4836998fb727
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2026-03-02 22:38:09 -0600

    use image data url

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -176,7 +176,7 @@ export const SlideShowContainer = () => {
                                 alt="not found"
                                 width={"100%"}
                                 height={"100%"}
-                                src={updatedData?.[currentImageIndex]?.imageData.imageDataUrl}
+                                src={updatedData?.[currentImageIndex]?.imageData.imageDataUrl + ".png"}
                             />}
                     </DemoPaper>
                     {imageCount > 1 && updatedData?.length > 1 && <IconButton aria-label="forwards" onClick={() => {
