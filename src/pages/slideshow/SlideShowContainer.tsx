0733d0e4b0b1ab9ab2b1b767f96d2a830e8e5079
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2026-03-03 16:11:30 -0600

    remove crop to test

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -175,13 +175,13 @@ export const SlideShowContainer = () => {
                             </IconButton>
                         }
                         {imageCount > 0 && updatedData?.length > currentImageIndex && updatedData?.length > 0 &&
-                            <ImageList sx={{ width: 500, height: 450 }}  cols={3} rowHeight={164}>
+                            <ImageList sx={{ width: "100%", height: "100%" }} cols={3} rowHeight={164}>
                                 {updatedData.map((item) => (
                                     <ImageListItem key={item.imageDataUrl}>
                                         <img
-                                            srcSet={`${item.imageDataUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                            src={`${item.imageDataUrl}?w=164&h=164&fit=crop&auto=format`}
-                                            alt={item.imageDataUrl}
+                                            srcSet={`${item.imageDataUrl}?w=164&h=164&auto=format&dpr=2 2x`}
+                                            src={`${item.imageDataUrl}?w=164&h=164&auto=format`}
+                                            alt={item.fileName}
                                             loading="lazy"
                                         />
                                     </ImageListItem>
