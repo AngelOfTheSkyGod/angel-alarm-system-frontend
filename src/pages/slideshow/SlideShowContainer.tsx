e2f95bd37699495adc9398bc31fdaeeb637aed3b
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2026-03-05 13:24:02 -0600

    trying to fix images on mobile not loading

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -219,7 +219,7 @@ export const SlideShowContainer = () => {
                                             </IconButton>
                                         }
                                         <img
-                                            src={item.imageDataUrl}
+                                            src={new URL(item.imageDataUrl, import.meta.url).href}
                                             alt={item.fileName}
                                             loading="lazy"
                                             style={{ width: "100%", height: "100%", objectFit: "cover" }}
