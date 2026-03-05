a7c5af610f13d46a0b223a3b33a9f56fd59abc13
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2026-03-05 00:12:02 -0600

    idk lol

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -101,10 +101,10 @@ export const SlideShowContainer = () => {
     }
 
     const removePicture = (item:SlideShowPictureData) => {
-        const currentImageIndex = updatedData?.findIndex((element) => item.imageDataUrl === element.imageDataUrl && item.fileName === element.fileName) || -1
-        const correctImageIndex = appData?.slideShowData?.findIndex((element) => item.imageDataUrl === element.imageDataUrl && item.fileName === element.fileName) || -1
+        const currentImageIndex = updatedData?.findIndex((element) => item.imageDataUrl == element.imageDataUrl && item.fileName == element.fileName) || -1
+        const correctImageIndex = appData?.slideShowData?.findIndex((element) => item.imageDataUrl == element.imageDataUrl && item.fileName == element.fileName) || -1
         console.log("item: ", item, "updated data:", updatedData, "app data:", appData?.slideShowData)
-        if (deletedImages.find((index) => index === correctImageIndex || correctImageIndex === -1)){
+        if (deletedImages.find((index) => index === correctImageIndex) || correctImageIndex === -1){
             return;
         }
         const list = updatedData !== null && updatedData.length > 0 ? [...updatedData] : [];
