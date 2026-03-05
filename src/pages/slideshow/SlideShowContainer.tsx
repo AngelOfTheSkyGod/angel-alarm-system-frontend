19f8dd13a683afdf9c04a7fbb093fc348a5a202b
AngelOfTheSkyGod <quinonesangel2000@gmail.com>
2026-03-05 15:44:17 -0600

    mobile caching improvement so images dont break

--- src/pages/slideshow/SlideShowContainer.tsx
+++ src/pages/slideshow/SlideShowContainer.tsx
@@ -220,7 +220,7 @@ export const SlideShowContainer = () => {
                                             </IconButton>
                                         }
                                         <img
-                                            src={new URL(item.imageDataUrl, import.meta.url).href}
+                                            src={`${item.imageDataUrl}?t=${Date.now()}`}
                                             alt={item.fileName}
                                             loading="lazy"
                                             style={{ width: "100%", height: "100%", objectFit: "cover" }}
